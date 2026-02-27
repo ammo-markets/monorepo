f00855b67d7d5e513be57e19e6e96cbc9a817991
chiranz <chiranpoudyal@gmail.com>
2026-02-27 21:05:13 +0530

    chore: update new contracts

--- packages/shared/src/config/index.ts
+++ packages/shared/src/config/index.ts
@@ -41,25 +41,25 @@ export const CONTRACT_ADDRESSES: {
   mainnet: NetworkAddresses;
 } = {
   fuji: {
-    manager: "0x5dB292eade6BEa9D710C54C5504d8400639dec25",
-    factory: "0xA802FE22E85461131Ca94C8bB85C1a36815aDe8D",
-    usdc: "0x270D06E53f943C6Dd69a2e51FEB07c420B3Ab146",
+    manager: "0xB275e81aDc8c0fFd8cb0d2c2142AcDBeb4A7B82c",
+    factory: "0xAbbf3fb509DA4FE46Ee65A3BE3A1BbE84E2Aa3e4",
+    usdc: "0xE073D724c03346202C5311aDEbca530aaB72299e",
     calibers: {
       "9MM": {
-        market: "0x5aFFA4CfF4920627C2061D211C44B1100E3a8Fe1",
-        token: "0x6a9753ffDbF5036991294Ce439a042dF834aCa62",
+        market: "0x0bff015EEA8C18674AB8d9B3f995E25359Da8Ab1",
+        token: "0x1f7999bc88090c7B397D4630442991a4E27Fb98F",
       },
       "556": {
-        market: "0xe082bDd7139eF03E8db1B9155f53aB60E5EF7e03",
-        token: "0x46951A49a4d73C70ba9A12bF82f4c4686a8b60E8",
+        market: "0x1094166b993c0D92912D298E6AD9E5170977e12C",
+        token: "0x11afaDEd9C8Fa5D19377251232Af30BeB57b04b6",
       },
       "22LR": {
-        market: "0xF1B4a75C77b8a9bFB52F9B800C3f26547eDD442b",
-        token: "0xFE10A09895Ab1AF20E5613c2e0715Aac56837ff5",
+        market: "0xc5DC672b3A72c1c6f78dBaF98d7A118bBCe18a79",
+        token: "0xac6A4F943845e7Fad9d7421756DAd1aceb57f4c7",
       },
       "308": {
-        market: "0x326b5AAc6C97918716264E307923c6D2c95cA440",
-        token: "0xa8685b36384b13d823bDeF75B96Ee83B6BF647A7",
+        market: "0x4b4DeBfCCCB60250007dB708f670CE74b62387AE",
+        token: "0x6Ea392b168BEFbC33776068c18ad8F4313cDdCeE",
       },
     },
   },
@@ -94,6 +94,6 @@ export const CONTRACT_ADDRESSES: {
  * to avoid scanning millions of empty blocks.
  */
 export const DEPLOYMENT_BLOCKS = {
-  fuji: BigInt(51699730),
+  fuji: BigInt(52220913),
   mainnet: BigInt(0), // Not yet deployed
 } as const;
